c74d05c7cbff99f89cc1c1e44be203cfe9baa1cf
changhui.xie <changhui.xie@kingstarfintech.com>
2019-11-02 18:39:44 +0800

    登录页面调整

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,75 +1,127 @@
 <template>
-<div style="margin:0; padding:0;">
-    <top-bar></top-bar>
-    <div class="container page-login">
-        <div class="row">
-            <div class="col-md-7 col-md-offset-2">
-                <section id="loginForm">
-                    <form action="/log-in?ReturnUrl=http%3A%2F%2Fwww.ituring.com.cn%2Farticle%2F506775" class="form-horizontal" method="post" role="form"><input name="__RequestVerificationToken" type="hidden" value="rEPcr2ghdJIzdq5WbwJxvfqnBKkVn9IKB1ql3Wr-_56XnYUQPNgEx7C7a54rW_o2GEfLz_gj-zUgKtLY9wDFuACpPbF835VelSeacPX2eP01" />                    
-                        <div class="row">
-                            <div class="col-md-7 col-md-offset-3">
-                                <div class="login-title">
-                                    <h4>会员登录</h4>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="Email" class="control-label col-md-3">邮箱</label>
-                            <div class="col-md-7">
-                                <input class="form-control" data-val="true" data-val-required="请输入Email/手机" id="Email" name="Email" placeholder="邮箱" type="text" value="" />
-                                <span class="field-validation-valid text-danger" data-valmsg-for="Email" data-valmsg-replace="true"></span>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="control-label col-md-3" for="Password">密码</label>
-                            <div class="col-md-7">
-                                <input class="form-control" data-val="true" data-val-required="请输入密码" id="Password" name="Password" placeholder="密码" type="password" />
-                                <span class="field-validation-valid text-danger" data-valmsg-for="Password" data-valmsg-replace="true"></span>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="col-md-7 col-md-offset-3">
-                                <div class="flat-checkbox">
-                                    <input id="RememberMe" type="checkbox" name="RememberMe" value="true" checked>
-                                    <label for="RememberMe">  下次自动登录</label>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="col-md-offset-3 col-md-7">
-                                <input type="submit" value="登 录" class="btn btn-primary" />
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-3">
-                                <div class="login-prompts">
-                                    <p>
-                                        您还没有账号？请点击这里<b><router-link to="/register">注册新账号</router-link></b>
-                                    </p>
-                                    <p>
-                                        您忘记密码了？请点击这里<b><a href="/forgot-passwprd">找回密码</a></b>
-                                    </p>
-                                    <p>
-                                        您忘记邮箱了？请点击这里<b><a href="/account/forget">查找账号</a></b>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </form>            
-                </section>
-            </div>
-            <div class="col-md-6">
-            </div>
-        </div>
-    </div>
-    <div id="footer">
-            <foot-bar></foot-bar>
-    </div>
-</div>
-</template>
+    <el-container  direction="vertical">
+        <top-bar></top-bar>
+        <el-main style="padding-top:90px">
+            <el-row>
+                <el-col :xs="24" :sm="6" :md="6" :lg="6" :offset="9">
+                    <el-form :rules="rules" ref="form" :model="form" label-width="80px" label-position="left" size="medium">
+                        <el-divider content-position="right"><h4>登录</h4></el-divider>
+                        <el-row>
+                            <el-col :xs="24" :sm="24" :md="24" :lg="24">
+                                <el-form-item label="邮箱" prop="email" >
+                                    <el-input v-model="form.email" placeholder="邮箱"></el-input>
+                                </el-form-item>
+                            </el-col>
+                        </el-row>
+                        <el-row>
+                            <el-col :xs="24" :sm="24" :md="24" :lg="24">
+                                <el-form-item label="密码" prop="password">
+                                    <el-input v-model="form.password" show-password  placeholder="密码"></el-input>
+                                </el-form-item>
+                            </el-col>
+                        </el-row>
+                        <el-row>
+                            <el-col :xs="24" :sm="24" :md="24" :lg="24">
+                                <el-form-item label="验证码" prop="verificationCode">
+                                    <el-input v-model="form.verificationCode"  placeholder="验证码"></el-input>
+                                </el-form-item>
+                            </el-col>
+                        </el-row>
 
+                        <el-row>
+                            <el-col :xs="24" :sm="24" :md="24" :lg="24">
+                                <el-button type="primary" style="width: 100%;" size="medium"  @click="formsubmit('form')">登录</el-button>
+                            </el-col>
+                        </el-row>
+
+                        <el-row>
+                            <el-col :xs="24" :sm="24" :md="24" :lg="24" style="padding-top:20px">
+                                <div class="pull-left">
+                                    没有账号？<b><el-link :underline="false" type="primary">注册</el-link></b>
+                                </div>
+                                <div class="pull-right">
+                                    <b><el-link :underline="false" type="primary" >找回密码</el-link></b>
+                                    <b style="margin-left:10px;"><el-link :underline="false" type="primary">查找账号</el-link></b>
+                                </div>
+                            </el-col>
+                        </el-row>
+                        
+                    </el-form>
+                </el-col>
+            </el-row>
+        </el-main>
+        <foot-bar></foot-bar>
+    </el-container>
+</template>
 <script>
+export default {
+    data() {
+        return {
+            query: {
+            },
+
+            form: {
+                email: "",
+                password: "",
+                verificationCode: "",
+            },
+            rules: {
+                email: [
+                    {required:true, message:'邮箱不能为空', trigger: 'blur'},
+                    {pattern:/^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/, message: "请输入正确的邮箱", trigger: 'blur'}
+                ],
+
+                password: [
+                    {required:true, message:'密码不能为空', trigger: 'blur'},
+                    {pattern:/^[a-zA-Z0-9_-]/, message: "密码由字母和数字组成", trigger: 'blur'}
+                ],
+
+                verificationCode: [
+                    {required:true, message:'图形验证码不能为空', trigger: 'blur'},
+                ],
+            },
+        }      
+    },
+    created() {
+    },
+
+    mounted(){
+    },
     
+    methods: {
+
+        checkForm: function (formName) {
+            var result  = false;
+            this.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        result = true;
+                    } else {
+                        result = false;
+                        return false;
+                    }
+            });
+            return result;
+        },
+
+        formsubmit: function (formName) {
+            this.$refs[formName].validate((valid) => {
+                if (valid) {
+                    this.onSubmit();
+                } else {
+                    console.log('error submit!!');
+                    return false;
+                }
+            });
+        },
+
+        onSubmit: async function () {
+            const result = await this.$axios.post('', this.form.bankList);
+            if (result.success) {
+               
+            }
+        },
+    },
+}
 </script>
 <style scoped>
 </style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,42 +1,42 @@
 <template>
     <el-container  direction="vertical">
         <top-bar></top-bar>
-        <el-main>
+        <el-main style="padding-top:90px">
             <el-row>
-                <el-col :xs="24" :sm="8" :md="8" :lg="8" :offset="8">
-                    <el-form :rules="rules" ref="form" :model="form" label-width="80px" label-position="right" size="medium">
+                <el-col :xs="24" :sm="6" :md="6" :lg="6" :offset="9">
+                    <el-form :rules="rules" ref="form" :model="form" label-width="80px" label-position="left" size="medium">
                         <el-divider content-position="right"><h4>新会员注册</h4></el-divider>
                         <el-row>
-                            <el-col :xs="24" :sm="16" :md="16" :lg="16">
+                            <el-col :xs="24" :sm="24" :md="24" :lg="24">
                                 <el-form-item label="邮箱" prop="email" >
                                     <el-input v-model="form.email" placeholder="邮箱"></el-input>
                                 </el-form-item>
                             </el-col>
                         </el-row>
                         <el-row>
-                            <el-col :xs="24" :sm="16" :md="16" :lg="16">
-                                <el-form-item label="昵称" prop="userName">
+                            <el-col :xs="24" :sm="24" :md="24" :lg="24">
+                                <el-form-item label="昵称" prop="nickname">
                                     <el-input v-model="form.nickname"  placeholder="昵称"></el-input>
                                 </el-form-item>
                             </el-col>
                         </el-row>
                         <el-row>
-                            <el-col :xs="24" :sm="16" :md="16" :lg="16">
-                                <el-form-item label="密码" prop="userIdNo">
-                                    <el-input v-model="form.password"  placeholder="密码"></el-input>
+                            <el-col :xs="24" :sm="24" :md="24" :lg="24">
+                                <el-form-item label="密码" prop="password">
+                                    <el-input v-model="form.password" show-password  placeholder="密码"></el-input>
                                 </el-form-item>
                             </el-col>
                         </el-row>
                         <el-row>
-                            <el-col :xs="24" :sm="16" :md="16" :lg="16">
-                                <el-form-item label="确认密码" prop="userIdNo">
-                                    <el-input v-model="form.repassword"  placeholder="确认密码"></el-input>
+                            <el-col :xs="24" :sm="24" :md="24" :lg="24">
+                                <el-form-item label="确认密码" prop="repassword">
+                                    <el-input v-model="form.repassword" show-password  placeholder="确认密码"></el-input>
                                 </el-form-item>
                             </el-col>
                         </el-row>
 
                         <el-row>
-                            <el-col :xs="24" :sm="16" :md="16" :lg="16">
+                            <el-col :xs="24" :sm="24" :md="24" :lg="24">
                                 <el-form-item label="验证码" prop="verificationCode">
                                     <el-input v-model="form.verificationCode"  placeholder="验证码"></el-input>
                                 </el-form-item>
@@ -44,15 +44,19 @@
                         </el-row>
 
                         <el-row>
-                            <el-col :xs="24" :sm="16" :md="16" :lg="16">
-                                <el-form-item label="" prop="verificationCode" style="text-align: left;">
+                            <el-col :xs="24" :sm="24" :md="24" :lg="24">
+                                <el-form-item label="" prop="readbook" style="text-align: left;">
                                     <el-checkbox v-model="form.readbook"  true-label="1" false-label="0">已阅读并同意</el-checkbox>
                                     <el-link type="warning" :underline="false"  @click="drawer = true">会员协议</el-link>
                                 </el-form-item>
                             </el-col>
                         </el-row>
+                        <el-row>
+                            <el-col :xs="24" :sm="24" :md="24" :lg="24">
+                                <el-button type="primary" style="width: 100%;"  @click="formsubmit('form')">提交</el-button>
+                            </el-col>
+                        </el-row>
                         
-                        <el-button type="primary" @click="formsubmit('form')">提交</el-button>
                     </el-form>
                 </el-col>
             </el-row>
@@ -63,6 +67,13 @@
 <script>
 export default {
     data() {
+        var checkRepassword = (rule, value, callback) => {
+            if (value !== this.form.password) {
+                callback(new Error('两次输入密码不一致!'))
+            } else {
+                callback()
+            }
+        }
         return {
             query: {
             },
@@ -76,6 +87,33 @@ export default {
                 readbook: ""
             },
             rules: {
+                email: [
+                    {required:true, message:'邮箱不能为空', trigger: 'blur'},
+                    {pattern:/^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/, message: "请输入正确的邮箱", trigger: 'blur'}
+                ],
+
+                nickname: [
+                    {required:true, message:'昵称不能为空', trigger: 'blur'},
+                    {pattern:/^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/, message: "请输入合法昵称", trigger: 'blur'}
+                ],
+
+                password: [
+                    {required:true, message:'密码不能为空', trigger: 'blur'},
+                    {pattern:/^[a-zA-Z0-9_-]/, message: "密码由字母和数字组成", trigger: 'blur'}
+                ],
+
+                repassword: [
+                    {required:true, message:'确认密码不能为空', trigger: 'blur'},
+                    {required:true, validator: checkRepassword, trigger: 'blur'},
+                ],
+
+                verificationCode: [
+                    {required:true, message:'图形验证码不能为空', trigger: 'blur'},
+                ],
+
+                readbook: [
+                    {required:true, message:'请阅读协议并同意', trigger: 'change'},
+                ],
                 
             },
         }      
